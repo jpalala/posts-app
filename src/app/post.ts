108964426aac8b8d800ab02b23eeee3c3329e293
Joe Palala <joe@palalamail.xyz>
2022-11-26 11:57:09 +0800

    add author in post model as optional

--- src/app/post.ts
+++ src/app/post.ts
@@ -4,7 +4,8 @@ export class Post {
     public userId: number,
     public id: number,
     public title: string,
-    public body: string
+    public body: string,
+    public author?: string
   ) {  }
 
 }
